f6ef601ef4782504b541e8153027e11cb22d5899
NouhailaErg <erguinouhaila@gmail.com>
2025-05-17 03:55:49 +0100

    Use Email with Gmail config working V

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -34,12 +34,12 @@ ADMIN_USER_EMAIL=config("ADMIN_USER_EMAIL", default=None)
 MANAGERS=[]
 ADMINS=[]
 if all([ADMIN_USER_NAME, ADMIN_USER_EMAIL]):
-    ADMIN +=[
+    ADMINS +=[
         (f'{ADMIN_USER_NAME}', f'{ADMIN_USER_EMAIL}')
     ]
     MANAGERS=ADMINS
 
-    
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
